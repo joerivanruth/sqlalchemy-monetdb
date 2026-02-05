89448340f9388f5be01cf38530e0b54711f0ddd4
Joeri van Ruth <jvr@pixie.blub.net>
2026-02-05 14:32:15 +0100

    temporarily suppress failing test

--- test/test_suite.py
+++ test/test_suite.py
@@ -13,3 +13,23 @@ class JSONTest:
                         "This is unexpected in this test.")
     def test_round_trip_custom_json(self):
         pass
+
+
+class LikeFunctionsTest:
+    @pytest.mark.skip(reason="monetdb container does not have PCRE")
+    def test_regexp_replace(self):
+        pass
+
+# FAILED test/test_suite.py::LikeFunctionsTest_monetdb+pymonetdb_11_55_2::test_regexp_replace - sqlalchemy.exc.OperationalError: (pymonetdb.exceptions.OperationalError) Database was compiled without PCRE support.
+# │  -# Failures
+# │  -class FetchLimitOffsetTest(FetchLimitOffsetTest):
+# │  -    @pytest.mark.skip(reason="This test does some LIMIT statements in "
+# │  -                      "the middle of the query, this is not supported.")
+# │  -    def test_limit_render_multiple_times(*args, **kwargs):
+# │  -        """
+# │  -        This test does some LIMIT statements in the middle of the query,
+# │  -        this is not supported.
+# │  -        """
+# │  -        pass
+# │  -
+# │  -
